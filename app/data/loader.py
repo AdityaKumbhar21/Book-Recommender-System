f54558706382b33a832dccc7ae2677cc706f12f8
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-07-22 11:52:34 +0530

    changed the dir struct

--- app/data/loader.py
+++ app/data/loader.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import pickle
+import os
+
+# Get the root directory of the project
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+ARTIFACTS_DIR = os.path.join(ROOT_DIR, 'artifacts')
 
 def load_pt():
-    with open('artifacts/pt.pkl', 'rb') as f:
+    with open(os.path.join(ARTIFACTS_DIR, 'pt.pkl'), 'rb') as f:
         return pickle.load(f)
 
 def load_similarity():
-    with open('artifacts/similarity_scores.pkl', 'rb') as f:
+    with open(os.path.join(ARTIFACTS_DIR, 'similarity_scores.pkl'), 'rb') as f:
         return pickle.load(f)
 
 def load_final_df():
-    return pd.read_csv('artifacts/final_df.csv')
+    return pd.read_csv(os.path.join(ARTIFACTS_DIR, 'final_df.csv'))
 
 def load_popular_df():
-    return pd.read_csv('artifacts/popular.csv')
-
+    return pd.read_csv(os.path.join(ARTIFACTS_DIR, 'popular.csv'))
