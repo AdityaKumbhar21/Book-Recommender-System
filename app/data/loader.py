2ada39c82fa9d7d748830bc0a6938d4518a9e703
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-07-22 11:30:17 +0530

    changed location for artifacts

--- app/data/loader.py
+++ app/data/loader.py
@@ -2,16 +2,16 @@ import pandas as pd
 import pickle
 
 def load_pt():
-    with open('../artifacts/pt.pkl', 'rb') as f:
+    with open('artifacts/pt.pkl', 'rb') as f:
         return pickle.load(f)
 
 def load_similarity():
-    with open('../artifacts/similarity_scores.pkl', 'rb') as f:
+    with open('artifacts/similarity_scores.pkl', 'rb') as f:
         return pickle.load(f)
 
 def load_final_df():
-    return pd.read_csv('../artifacts/final_df.csv')
+    return pd.read_csv('artifacts/final_df.csv')
 
 def load_popular_df():
-    return pd.read_csv('../artifacts/popular.csv')
+    return pd.read_csv('artifacts/popular.csv')
 
